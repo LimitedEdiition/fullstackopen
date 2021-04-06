df56ff742c09a54c6a7b949d092d851dceb300ca
Simon <ayshunpryde@gmail.com>
2021-04-06 16:32:02 -0400

    FSO Part 1 E.1.12

--- src/App.js
+++ src/App.js
@@ -1,75 +1,28 @@
 import React, { useState } from 'react'
 
-/*const Button = (props) => {
-  return (
-    <button onClick={props.handleClick}>
-      {props.text}
-    </button>
-  )
-}*/
-
-const Button = ({handleClick, text}) => <div><button onClick={handleClick}>{text}</button></div>
-
-const Statistic = ({value}) => <div>{value}</div>
-
-const Statistics = ({good, bad, neutral}) => {
-  if((good === 0) && (bad === 0) && (neutral === 0)) {
-    return (
-      <div>
-        No feedback given
-      </div>
-    )
-  }
-
-  else{
-    let total = good + bad + neutral
-    let avg = (good + bad*-1)/total
-    let pos = good/total
-    return(
-      <div>
-        <table>
-          <tbody>
-            <tr>
-              <td><Statistic value="Good"/></td>
-              <td><Statistic value="Neutral"/></td>
-              <td><Statistic value="Bad"/></td>
-              <td><Statistic value="All"/></td>
-              <td><Statistic value="Average"/></td>
-              <td><Statistic value="Positive"/></td>
-            </tr>
-            <tr>
-              <td><Statistic value={good}/></td>
-              <td><Statistic value={neutral}/></td>
-              <td><Statistic value={bad}/></td>
-              <td><Statistic value={total}/></td>
-              <td><Statistic value={avg}/></td>
-              <td><Statistic value={pos}/></td>
-            </tr>
-          </tbody>
-        </table>
-      </div>
-    )
-  }
-}
+const Button = ({text, genRandom}) => <div><button onClick={genRandom}>{text}</button></div>
 
 const App = () => {
-  // save clicks of each button to its own state
-  const [good, setGood] = useState(0)
-  const [neutral, setNeutral] = useState(0)
-  const [bad, setBad] = useState(0)
+  const anecdotes = [
+    'If it hurts, do it more often',
+    'Adding manpower to a late software project makes it later!',
+    'The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
+    'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
+    'Premature optimization is the root of all evil.',
+    'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.'
+  ]
+   
+  const [selected, setSelected] = useState(0)
 
-  const clickGood = () => setGood(good+1)
-  const clickNeutral = () => setNeutral(neutral+1)
-  const clickBad = () => setBad(bad+1)
+  const genRandom = () => {
+    let num = Math.floor(Math.random()*anecdotes.length)
+    setSelected(num)
+  }
 
   return (
     <div>
-      Give Feedback
-      <Button handleClick={clickGood} text='Good' />
-      <Button handleClick={clickNeutral} text='Neutral' />
-      <Button handleClick={clickBad} text='Bad' />
-      Statistics
-      <Statistics good={good} neutral={neutral} bad={bad}/>
+      {anecdotes[selected]}
+      <Button genRandom={genRandom} text="Next Anecdote"/>
     </div>
   )
 }
